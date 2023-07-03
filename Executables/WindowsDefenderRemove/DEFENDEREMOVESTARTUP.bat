77ee3c02120515fd9e27992d770c2e6253c21bef
Xander <111686716+XanderFromFortnite@users.noreply.github.com>
2023-07-03 15:33:58 -0500

    Delete Executables/WindowsDefenderRemove directory

--- Executables/WindowsDefenderRemove/DEFENDEREMOVESTARTUP.bat
+++
@@ -1,5 +0,0 @@
-:: Registry Remove of Windows Defender
-FOR /R %%f IN (GalleryInc.MelodyScript.DefenderRemover.DisablerScript\*.reg Remove_defender\*.reg GalleryInc.MelodyScript.DefenderRemover.Extras\*.reg) DO regedit.exe /s "%%f"
-FOR /R %%f IN (GalleryInc.MelodyScript.DefenderRemover.DisablerScript\*.reg Remove_defender\*.reg GalleryInc.MelodyScript.DefenderRemover.Extras\*.reg) DO PowerRun.exe regedit.exe /s "%%f"
-start /B /wait PowerRun.exe "DEFENDEREMOVE.bat"
-exit
